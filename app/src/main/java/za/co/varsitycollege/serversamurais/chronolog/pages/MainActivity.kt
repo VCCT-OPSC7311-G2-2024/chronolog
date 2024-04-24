f5fefcd4b568bd50a76a436940767ca308f28599
Aidan Naidoo <ST10109482@vcconnect.edu.za>
2024-04-25 01:22:29 +0200

    Added a new button nav bar with different fragments

--- app/src/main/java/za/co/varsitycollege/serversamurais/chronolog/pages/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/serversamurais/chronolog/pages/MainActivity.kt
@@ -5,17 +5,47 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import za.co.varsitycollege.serversamurais.chronolog.HomePage
 import za.co.varsitycollege.serversamurais.chronolog.R
+import za.co.varsitycollege.serversamurais.chronolog.Stats
+import za.co.varsitycollege.serversamurais.chronolog.TimeSheet
+import za.co.varsitycollege.serversamurais.chronolog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Set up edge-to-edge handling
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Replace the initial fragment with HomePage
+        replaceFragment(HomePage())
+
+        // Set up bottom navigation item listener
+        binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.homeNavID -> replaceFragment(HomePage())
+                R.id.timesheetNavID -> replaceFragment(TimeSheet())
+                R.id.statsNavID -> replaceFragment(Stats())
+            }
+            true // Indicate that the item selection event has been handled
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frameLayout, fragment)
+            .commit()
     }
 }
---
+++ app/src/main/java/za/co/varsitycollege/serversamurais/chronolog/pages/StatsActivty.kt
@@ -0,0 +1,4 @@
+package za.co.varsitycollege.serversamurais.chronolog.pages
+
+class StatsActivty {
+}
